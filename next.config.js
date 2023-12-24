15eda2c626bc21d773957bfd439e4576a5a75abf
Dan <slashui@live.cn>
2023-12-24 13:47:06 +0800

    delete all

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-const withNextIntl = require("next-intl/plugin")(
-  // This is the default (also the `src` folder is supported out of the box)
-  "libs/i18n/i18n.js"
-);
-
-module.exports = withNextIntl(nextConfig);

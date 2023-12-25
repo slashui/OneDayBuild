b6c2a3cd97cf8d8dcc193c342025b64bfa103ead
Dan <slashui@live.cn>
2023-12-25 17:21:47 +0800

    kk

--- components/EmailTemplate.jsx
+++ components/EmailTemplate.jsx
@@ -1,6 +1,7 @@
 export const EmailTemplate = ({OTP}) => (
     <div>
-    <h1>Welcome,<span style="color:#ff0000">{OTP}</span>!</h1>
+    <h1>Welcome,{OTP}!</h1>
+    <img src="https://one-day-build.vercel.app/logo.png" />
     <p>Thank you for sending me a message. I will get back with you as soon as I can.</p>
     </div>
 );

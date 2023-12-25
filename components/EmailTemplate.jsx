c57b0e0e9ab538bd29ab6cf79bea1b00a3f9462f
Dan <slashui@live.cn>
2023-12-25 17:12:27 +0800

    Update EmailTemplate.jsx

--- components/EmailTemplate.jsx
+++ components/EmailTemplate.jsx
@@ -1,6 +1,6 @@
 export const EmailTemplate = ({OTP}) => (
     <div>
-    <h1>Welcome,{OTP}!</h1>
+    <h1>Welcome,<span style="color:#ff0000">{OTP}</span>!</h1>
     <p>Thank you for sending me a message. I will get back with you as soon as I can.</p>
     </div>
 );

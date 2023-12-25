ba506aab46839ec6863dfccc50d460821bcf1d7b
Dan <slashui@live.cn>
2023-12-25 16:32:38 +0800

    Update route.js

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -5,8 +5,13 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request) {
   try {
+    console.log("Hello");
+    console.log(request);
     const body = await request.json();   
     const { email, subject, message , OTP } = body;
+    console.log(body);
+    console.log(email);
+    console.log(message);
     const data = await resend.emails.send({
       from: 'Acme <onboarding@loveai.guru>',
       to: email,
